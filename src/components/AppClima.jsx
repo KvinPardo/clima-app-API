ef289d6712d0b9843b0ea8305541f214e2724a0f
KvinPardo <kevin.pardov25@gmail.com>
2022-11-01 17:13:12 -0300

    terminado

---
+++ src/components/AppClima.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import useClima from '../hooks/useClima'
+import Formulario from './Formulario'
+import Loading from './Loading'
+import Resultado from './Resultado'
+
+const AppClima = () => {
+
+  const {resultado, cargando} = useClima()
+
+  return (
+    <>
+        <main className='dos-columnas'>
+            <Formulario />
+
+            {cargando ? <Loading /> : resultado?.name && <Resultado />}
+            
+        </main>
+    </>
+  )
+}
+
+export default AppClima
